4d6f990c14a2f60904aeeb693e9a2bea39ab34d2
Karan Munyal <munyal.karan@gmail.com>
2016-10-23 12:19:08 -0700

    The changes made in the text part

--- index.ios.js
+++ index.ios.js
@@ -11,10 +11,11 @@ import {
   AppRegistry,
   StyleSheet,
   MapView,
+  Text,
   View
 } from 'react-native';
 
-var Weather= React.createClass({
+var Weather = React.createClass({
   getInitialState: function() {
     return{
       pin:{
@@ -27,35 +28,71 @@ var Weather= React.createClass({
     };
   },
   render:function(){
-      return <MapView
-      annotations={[this.state.pin]}
-      onRegionChangeComplete={this.onRegionChangeComplete}// need to find the long and lattitude of the place at the center of the screen
-      // created a function // this will come to know the place when the touch is removed and will find the place
-       style={styles.map}>
-       </MapView>
-  },
+      return <View style={styles.container}>
+                <MapView
+                showsCompass true
+                annotations={[this.state.pin]}
+                onRegionChangeComplete={this.onRegionChangeComplete}// need to find the long and lattitude of the place at the center of the screen
+                // created a function // this will come to know the place when the touch is removed and will find the place
+                 style={styles.map}>
+                 </MapView>
+                 <View style={styles.textWrapper}>
+                 <Text style={styles.text}>{this.state.city}</Text>
+                 <Text style={styles.text}>{this.state.temperature}</Text>
+                 <Text style={styles.text}>{this.state.description}</Text>
+                 </View>
+            </View>
+          },
 
-  onRegionChangeComplete: function(region){
+  onRegionChangeComplete: function(region) {
       this.setState({
-                      pin:{
+                      pin: {
                         latitude:region.latitude,
                         longitude:region.longitude
-                          }
+                      }
                     });
+                    console.log(region.latitude);
+                    console.log(region.longitude);
+
 // whenever the person stops draging the screen it gets a location so here we will do an API called
 
-Api(region.latitude,region.longitude) // with this we are just making an Api call
+/*Api(region.latitude,region.longitude) // with this we are just making an Api call
 // now we need to fetch the data from the Api
 .then((data) => { // now we got the data so re render it on the screen using the concept of state
                   console.log(data);
                   this.setState(data);
+                })
+                .catch(error)=>{
+                console.log("Api call error");
+                   alert(error.message);
+});
+*/
+      Api(region.latitude, region.longitude)
+                .then((data) => {
+                    console.log(data);
+                    this.setState(data);
+                }).catch((error)=>{
+                   console.log("Api call error");
+                   alert(error.message);
                 });
-  }
+    }
 });
 
 var styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'stretch',
+      backgroundColor: 'white'
+  },
   map: {
-    flex:1
+    flex: 2,
+    marginTop: 30
+  },
+  textWrapper:{
+    flex: 1,
+    alignItems: 'center',
+  //  fontSize:30
   }
 });
 AppRegistry.registerComponent('weather', () => Weather);
--- src/api.js
+++ src/api.js
@@ -1,21 +1,26 @@
-var rootUrl ='http://api.openweathermap.org/data/2.5/weather?appid=92e668b38f38590c397dbc98914a684c';
-
+var rootUrl ='http://api.openweathermap.org/data/2.5/weather?appid=33f8531d61d22aa4e8d32279bc05e3e9';
+var _ = require('lodash');
 var kelvinToF = function(kelvin){
-return Math.round((kelvin - 273.5) * 1.8 + 32)+ '˚F'
+  return Math.round((kelvin - 273.5) * 1.8 + 32)+ '˚F'
 };
 
 module.exports = function(latitude,longitude){
-  var url = `${rootUrl}&lat=${latitude}$lon=${longitude}`;
+  var url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
 
   return fetch(url)// make a request to the API and it returns a promise
-  .then(function(response){//gets the response but cant be used directly
-      return response.json();
-  })
-  .then(function(json){// so json is called
-      return {
-        city:json.name,//as name is the proper to display the city name in he json
-        temperature:kelvinToF(json.main.temp),
-        description:json.weather[0].description
-      }
-    });
-}
+          .then(function(response){//gets the response but cant be used directly
+              return response.json();
+          })
+          .then(function(json){// so json is called
+              return {
+                city: (json.name),//as name is the proper to display the city name in he json
+                temperature:kelvinToF(json.main.temp),
+                description: _.capitalize(json.weather[0].description)
+              }
+          })
+          .catch(function(error) {
+              console.log('There has been a problem with your fetch operation: ' + error.message);
+             // ADD THIS THROW error
+              throw error;
+        });
+};
